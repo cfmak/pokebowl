a9ce7695a6c9c1b416e50ec2b3191437c7efe0d8
Geoffrey Mak <geoffrey.mak@lemmatrix.com>
2021-04-23 09:37:02 -0500

    delete comments

--- pokebowl/src/test/scala/com/geoffreymak/MixerRoutesSpec.scala
+++ pokebowl/src/test/scala/com/geoffreymak/MixerRoutesSpec.scala
@@ -1,7 +1,5 @@
 package com.geoffreymak
 
-//#mixer-routes-spec
-//#test-top
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
@@ -9,30 +7,21 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-//#set-up
 class MixerRoutesSpec extends AnyWordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
-  //#test-top
 
   lazy val testKit = ActorTestKit()
   implicit def typedSystem = testKit.system
   override def createActorSystem(): akka.actor.ActorSystem =
     testKit.system.classicSystem
 
-  // Here we need to implement all the abstract members of UserRoutes.
-  // We use the real UserRegistryActor to test it while we hit the Routes,
-  // but we could "mock" it by implementing it in-place or by using a TestProbe
-  // created with testKit.createTestProbe()
   val mixerRegistry = testKit.spawn(MixerRegistry())
   lazy val routes = new MixerRoutes(mixerRegistry).routes
 
-  // use the json formats to marshal and unmarshall objects in the test
   import JsonFormats._
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-  //#set-up
 
   "MixerRoutes" should {
     "return deposit address (POST /mixer)" in {
-      // note that there's no need for the host part in the uri:
       val data =
         """
           |{
@@ -52,10 +41,8 @@ class MixerRoutesSpec extends AnyWordSpec with Matchers with ScalaFutures with S
       request ~> routes ~> check {
         status should ===(StatusCodes.Created)
 
-        // we expect the response to be json:
         contentType should ===(ContentTypes.`application/json`)
 
-        // and no entries should be in the list:
         entityAs[DepositAddress]
       }
     }
@@ -71,9 +58,4 @@ class MixerRoutesSpec extends AnyWordSpec with Matchers with ScalaFutures with S
       }
     }
   }
-  //#actual-test
-
-  //#set-up
 }
-//#set-up
-//#mixer-routes-spec
